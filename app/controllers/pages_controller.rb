4e4b367327a1c6f36834e48be2c53e4c1707a1ae
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-12-23 00:12:04 +0700

    bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,17 @@ class PagesController < ApplicationController
 	
 	@q = Station.search(h)		
 	@stations = @q.result(:distinct => true)
-	
-	@city = City.find(4400)	
-	if (params[:q] != nil && params[:q][:city_name_cont] != nil)
-		@city = City.where(["name like ?", params[:q][:city_name_cont]]).first
+		
+	if (params[:q] != nil && params[:q][:city_name_cont] != nil)				
+		@city = City.where(["name like ?", params[:q][:city_name_cont]]).first				
 	end	
+		
+	if (@city == nil) 
+		@city = City.find(4400)		# set Moscow
+	end
 	
-	@price = Price.where(["region_id = ?", @city.region.id]).first
+	@price = Price.where(["region_id = ?", @city.region.id]).first	
+	
   end
 
 end
